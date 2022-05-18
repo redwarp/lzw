f4742002099be66fa4dac63c1f38f992f7a81a86
Benoit Vermont <redwarp@gmail.com>
2022-05-18 11:19:29 +0200

    Use a string array instead of tree

--- lzw/src/decoder.rs
+++ lzw/src/decoder.rs
@@ -1,5 +1,5 @@
 use std::{
-    fmt::Display,
+    fmt::{Debug, Display},
     io::{Read, Write},
 };
 
@@ -14,7 +14,7 @@ pub enum DecodingError {
 impl Display for DecodingError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            DecodingError::Io(error) => error.fmt(f),
+            DecodingError::Io(error) => std::fmt::Display::fmt(&error, f),
             DecodingError::Lzw(message) => f.write_str(message),
         }
     }
@@ -28,197 +28,100 @@ impl From<std::io::Error> for DecodingError {
     }
 }
 
+// Idea: Have one big vec of [u8]. Store the strings there, and keep a vec of [start, length] for each word.
 #[derive(Debug, Clone)]
-enum Depth {
-    Root,
-    Child { depth: u16, parent: u16 },
-}
-
-impl Depth {
-    fn get_depth(&self) -> u16 {
-        match self {
-            Depth::Root => 0,
-            Depth::Child { depth, parent: _ } => *depth,
-        }
-    }
-
-    fn get_parent(&self) -> Option<u16> {
-        match self {
-            Depth::Root => None,
-            Depth::Child { depth: _, parent } => Some(*parent),
-        }
-    }
-}
-
-#[derive(Debug, Clone)]
-enum Children {
-    Zero,
-    One(u8, u16),
-    Many(Vec<u16>),
-}
-
-struct TreeNode {
-    k: u8,
-    root: u8,
-    depth: Depth,
-    children: Children,
-}
-
-impl TreeNode {
-    fn root(k: u8) -> Self {
-        Self {
-            k,
-            root: k,
-            depth: Depth::Root,
-            children: Children::Zero,
-        }
-    }
+struct Word {
+    start: usize,
+    end: usize,
 }
 
 struct Tree {
     code_size: u8,
-    buffer: [u8; 4086],
-    nodes: Vec<TreeNode>,
-}
-
-struct TreeIterator<'a> {
-    depth: usize,
-    size: usize,
-    next_word: Option<u16>,
-    nodes: &'a Vec<TreeNode>,
-}
-
-impl<'a> TreeIterator<'a> {
-    fn new(nodes: &'a Vec<TreeNode>, word: u16) -> Self {
-        let depth = nodes[word as usize].depth.get_depth() as usize;
-        let next_word = Some(word);
-        Self {
-            depth,
-            size: depth + 1,
-            next_word,
-            nodes,
-        }
-    }
-}
-
-impl<'a> Iterator for TreeIterator<'a> {
-    type Item = u8;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        match self.next_word {
-            Some(next_word) => {
-                let tree_node = &self.nodes[next_word as usize];
-                self.next_word = tree_node.depth.get_parent();
-
-                self.size -= 1;
-
-                Some(tree_node.k)
-            }
-            None => None,
-        }
-    }
-
-    fn size_hint(&self) -> (usize, Option<usize>) {
-        let size = self.size;
-        (size, Some(size))
-    }
-}
-
-impl<'a> ExactSizeIterator for TreeIterator<'a> {
-    fn len(&self) -> usize {
-        self.size
-    }
+    strings: Vec<u8>,
+    words: Vec<Word>,
 }
 
 impl Tree {
     fn new(code_size: u8) -> Self {
-        let buffer = [0; 4086];
-        let nodes = Vec::with_capacity(1 << (code_size + 1));
+        let strings = (0..1 << code_size).collect();
+        let mut words = Vec::with_capacity(1 << (code_size + 1));
+        words.extend((0..1 << code_size).map(|i| Word {
+            start: i,
+            end: i + 1,
+        }));
+        words.push(Word { start: 0, end: 0 });
+        words.push(Word { start: 0, end: 0 });
 
         Self {
             code_size,
-            buffer,
-            nodes,
+            strings,
+            words,
         }
     }
 
     fn clear(&mut self) {
-        self.nodes.clear();
-        self.nodes
-            .extend((0..1 << self.code_size).map(TreeNode::root));
-        self.nodes.push(TreeNode::root(0));
-        self.nodes.push(TreeNode::root(0));
+        self.strings.resize(1 << self.code_size, 0);
+        self.words
+            .resize((1 << self.code_size) + 2, Word { start: 0, end: 0 });
     }
 
-    fn write_word_if_found<W: Write>(
-        &mut self,
-        word: u16,
-        write: &mut W,
-    ) -> Result<Option<u8>, std::io::Error> {
-        if (word as usize) < self.nodes.len() {
-            let iterator = TreeIterator::new(&self.nodes, word);
-            let depth = iterator.depth;
-            for (i, k) in iterator.enumerate() {
-                self.buffer[depth - i] = k;
-            }
-            write.write_all(&self.buffer[..=depth])?;
-
-            Ok(Some(self.buffer[0]))
+    fn find_word(&self, code: u16) -> Option<&[u8]> {
+        if let Some(word) = self.words.get(code as usize) {
+            Some(&self.strings[word.start..word.end])
         } else {
-            Ok(None)
+            None
         }
     }
 
     fn add(&mut self, prefix: u16, k: u8) -> u16 {
-        let new_index = self.nodes.len() as u16;
-        let prefix_index = prefix as usize;
-
-        let mut parent_node = &mut self.nodes[prefix_index];
-
-        match &mut parent_node.children {
-            Children::Zero => {
-                parent_node.children = Children::One(k, new_index);
+        let prefix_word = &self.words[prefix as usize];
+        let new_word = if prefix_word.end == self.strings.len() {
+            // Adding to last inserted word.
+            self.strings.push(k);
+            Word {
+                start: prefix_word.start,
+                end: prefix_word.end + 1,
             }
-            Children::One(other_k, other_index) => {
-                let mut children = vec![u16::MAX; 1 << self.code_size];
-                children[*other_k as usize] = *other_index;
-                children[k as usize] = new_index;
-                parent_node.children = Children::Many(children);
-            }
-            Children::Many(children) => {
-                children[k as usize] = new_index;
-            }
-        };
-        let root = parent_node.root;
-        let depth = Depth::Child {
-            depth: parent_node.depth.get_depth() + 1,
-            parent: prefix,
+        } else {
+            let start = self.strings.len();
+            self.strings
+                .extend_from_within(prefix_word.start..prefix_word.end);
+            self.strings.push(k);
+            let end = self.strings.len();
+            Word { start, end }
         };
 
-        self.nodes.push(TreeNode {
-            k,
-            root,
-            depth,
-            children: Children::Zero,
-        });
-        new_index
+        let new_index = self.words.len();
+        self.words.push(new_word);
+        new_index as u16
     }
+}
 
-    fn root_for(&self, prefix: u16) -> u8 {
-        self.nodes[prefix as usize].root
+impl Debug for Tree {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for (i, word) in self.words.iter().enumerate() {
+            f.write_fmt(format_args!(
+                "{i} - {:?}\n",
+                &self.strings[word.start..word.end]
+            ))?;
+        }
+        Ok(())
     }
 }
 
 pub struct Decoder {
     code_size: u8,
     endianness: Endianness,
+    table: Tree,
 }
 
 impl Decoder {
     pub fn new(code_size: u8, endianness: Endianness) -> Self {
+        let table = Tree::new(code_size);
         Self {
             code_size,
             endianness,
+            table,
         }
     }
 
@@ -229,7 +132,7 @@ impl Decoder {
 
         let clear_code = 1 << self.code_size;
         let end_of_information = (1 << self.code_size) + 1;
-        let mut tree = Tree::new(self.code_size);
+        let tree = &mut self.table;
 
         let expected_clear_code = bit_reader.read(read_size)?;
         if expected_clear_code != clear_code {
@@ -257,15 +160,18 @@ impl Decoder {
             } else if k == end_of_information {
                 break 'read_loop;
             } else {
-                let index_of_new_entry =
-                    if let Some(first_k) = tree.write_word_if_found(k, &mut into)? {
-                        tree.add(current_prefix, first_k)
-                    } else {
-                        let first_k = tree.root_for(current_prefix);
-                        tree.write_word_if_found(current_prefix, &mut into)?;
-                        into.write_all(&[first_k])?;
-                        tree.add(current_prefix, first_k)
-                    };
+                let extra_char = if let Some(string) = tree.find_word(k) {
+                    into.write_all(string)?;
+                    string[0]
+                } else {
+                    let word = tree.find_word(current_prefix).expect("Should be set");
+
+                    let extra_char = word[0];
+                    into.write_all(word)?;
+                    into.write_all(&[extra_char])?;
+                    extra_char
+                };
+                let index_of_new_entry = tree.add(current_prefix, extra_char);
 
                 if index_of_new_entry == (1 << read_size) - 1 {
                     read_size += 1;
